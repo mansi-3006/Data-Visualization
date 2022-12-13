818d301bd2b4d9b2264413920be8c8ebb0e4b6c2
Mansi Patel <47717106+mansi-3006@users.noreply.github.com>
2022-12-13 14:56:03 -0500

    Add files via upload

--- data-viz-streamlit-main/app.py
+++ data-viz-streamlit-main/app.py
@@ -83,5 +83,10 @@ with st.container():
         - Since its inception in 2008, Airbnb has disrupted the traditional hospitality industry as more travellers decide to use Airbnb as their primary means of accommodation. Airbnb offers travellers a more unique and personalized way of accommodation and experience.
         - This dataset has one file- AB_US_2020.csv which has columns describing features such as host id, hostname, listing id, listing name, latitude and longitude of listing, the neighbourhood, price, room type, minimum number of nights, number of reviews, last review date, reviews per month, availability, host listings and city.
         - This dataset is a compilation of multiple datasets found on Inside Airbnb.
+        - Inspiration : 
+            - What regional gap exists in listing prices? What US communities are highly rated by tourists?
+            - Are there any correlations between rental prices and demand?
+            - Can the free-text section of the reviews be used to determine any common patterns? What aspects of renting are people’s favorites, and what aspects do they detest?What aspects of renting are people’s favorites, and what aspects do they detest?
+            - What different sorts of housing markets are there in the United States? Do they differ based on the location?
         """
     )
